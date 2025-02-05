79c66a1ff6c1075f394cbe4db6be4c340881473b
Yuu-Yur <gkcjdqls@gmail.com>
2025-02-05 13:00:31 +0900

    네비게이트 수정완료, 로그아웃기능 구현중

--- ReactTeam/src/pages/MainPage.jsx
+++ ReactTeam/src/pages/MainPage.jsx
@@ -20,17 +20,34 @@ const MainPage = () => {
   const category = useParams().category || 'main'; // url parameter 받기
   // data 즉 token 이 없으면 login 으로 리다이렉트
   const { deviceId } = usePlayback();
-  if (!data) {
+  if (!data && !tokenData) {
     return <Navigate to="/login" replace={true} />;
   }
+
+  console.log('localStorage 토큰:', localStorage.getItem('token'));
+  console.log('sessionStorage 토큰:', sessionStorage.getItem('token'));
 
   // 일단 categories 컴포넌트와 api 호출용 컴포넌트 부름
   return (
     <div>
       <Categories />
-      <Api category={category} />
+      {tokenData && <Api category={category} />}
       {tokenData && <SpotifyPlayer />}
       {deviceId && <PlaybackControls />}
+      <button
+        onClick={() => {
+          const logoutWindow = window.open(
+            'https://accounts.spotify.com/logout',
+            '_blank',
+            'width=700,height=500',
+          );
+          setTimeout(() => {
+            window.location.reload();
+          }, 1000);
+        }}
+      >
+        로그아웃 버튼
+      </button>
     </div>
   );
 };
