6310e2fac89b83091a2483215036642251a18503
leesanghwa1997 <zlqlcm1997@gmail.com>
2025-02-05 12:19:30 +0900

    middle 적용전 커밋

--- ReactTeam/src/components/Main.jsx
+++ ReactTeam/src/components/Main.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { useLocation, useParams, Navigate } from 'react-router-dom';
 import GetSeveralBrowseCategories from '../components/GetSeveralBrowseCategories';
 import MyPlaylist from '../components/MyPlaylist';
-import FeaturedPlaylists from '../components/FeaturedPlaylists'; // 추가
+import FeaturedPlaylists from '../components/FeaturedPlaylists';
+import NewReleases from '../components/NewReleases'; // 추가
 import { usePlayback } from '../contextAPI/PlaybackProvider';
 
 const Main = () => {
@@ -21,10 +22,13 @@ const Main = () => {
             {/* <GetSeveralBrowseCategories authorization={`${data.token_type} ${data.access_token}`} /> */}
 
             <h1>추천 플레이리스트</h1>
-            <FeaturedPlaylists authorization={`${data.token_type} ${data.access_token}`} /> {/* 추가 */}
+            <FeaturedPlaylists authorization={`${data.token_type} ${data.access_token}`} />
 
             <h1>내 플레이 리스트</h1>
             <MyPlaylist authorization={`${data.token_type} ${data.access_token}`} />
+
+            <h1>최신 발매 앨범</h1>
+            <NewReleases authorization={`${data.token_type} ${data.access_token}`} /> {/* 추가 */}
         </div>
     );
 };
