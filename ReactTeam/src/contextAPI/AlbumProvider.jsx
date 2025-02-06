b0a2ae75a7e405488aa33443f21eec577e8f3707
Yuu-Yur <gkcjdqls@gmail.com>
2025-02-06 18:08:38 +0900

    새로고침시 데이터 유지

--- ReactTeam/src/contextAPI/AlbumProvider.jsx
+++ ReactTeam/src/contextAPI/AlbumProvider.jsx
@@ -1,17 +1,29 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 
 // AlbumContext 생성
 export const AlbumContext = createContext();
 
 const AlbumProvider = ({ children }) => {
-    // 앨범 데이터 상태 관리
-    const [albumResults, setAlbumResults] = useState([]);
+  // 앨범 데이터 상태 관리
+  const [albumResults, setAlbumResults] = useState(() => {
+    sessionStorage.getItem('albumData')
+      ? JSON.parse(sessionStorage.getItem('albumData'))
+      : null;
+  });
 
-    return (
-        <AlbumContext.Provider value={{ albumResults, setAlbumResults }}>
-            {children}
-        </AlbumContext.Provider>
-    );
+  useEffect(() => {
+    if (albumResults) {
+      sessionStorage.setItem('albumData', JSON.stringify(albumResults));
+    } else {
+      sessionStorage.removeItem('albumData'); // 로그아웃 시 제거
+    }
+  }, [albumResults]);
+
+  return (
+    <AlbumContext.Provider value={{ albumResults, setAlbumResults }}>
+      {children}
+    </AlbumContext.Provider>
+  );
 };
 
 export default AlbumProvider;
