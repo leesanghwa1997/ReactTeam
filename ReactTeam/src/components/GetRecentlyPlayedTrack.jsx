b56811554ff4b652a60fb5ec48c5635cf6a9b12d
Yuu-Yur <gkcjdqls@gmail.com>
2025-02-10 09:51:03 +0900

    Revert "최근 재생, 무한스크롤"
    This reverts commit ca1a126addbea5ca9f2cdb448d5ec102e35f126f.

--- ReactTeam/src/components/GetRecentlyPlayedTrack.jsx
+++ ReactTeam/src/components/GetRecentlyPlayedTrack.jsx
@@ -1,60 +1,14 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { FreeMode, Pagination } from 'swiper/modules';
 import { Link } from 'react-router-dom';
 import { usePlayback } from '../contextAPI/PlaybackProvider';
 import useScrollData from '../lib/useScrollData';
-import styled from 'styled-components';
-
-const AlbumContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  gap: 16px;
-  overflow-y: auto;
-  max-height: 80vh;
-
-  &::-webkit-scrollbar {
-    display: none;
-  }
-`;
-
-const AlbumItem = styled.div`
-  width: calc(25% - 12px); /* 가로 4개 배치, 간격 고려 */
-  cursor: pointer;
-`;
-
-const AlbumImageThumb = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 20px;
-  overflow: hidden;
-  cursor: pointer;
-`;
-
-const AlbumImage = styled.img`
-  width: 100%;
-  transition: transform 0.6s cubic-bezier(0.16, 1, 0.3, 1);
-
-  &:hover {
-    transform: scale(1.05);
-  }
-`;
-
-const AlbumInfo = styled.div`
-  margin-top: 8px;
-`;
-
-const ArtistName = styled.span`
-  font-size: 14px;
-  color: #555;
-`;
 
 const GetRecentlyPlayedTrack = ({ authorization }) => {
-  const endpoint = `https://api.spotify.com/v1/me/player/recently-played?limit=12`;
+  const endpoint = `https://api.spotify.com/v1/me/player/recently-played?limit=10&before=${Date.now()}`;
   const { playUri } = usePlayback(); // 트랙 재생 함수
   const { data, handleReachEnd } = useScrollData(endpoint, authorization);
-  const listRef = useRef(null);
 
   const seen = new Set();
   const uniqueData = data.filter((item) => {
@@ -62,56 +16,45 @@ const GetRecentlyPlayedTrack = ({ authorization }) => {
     seen.add(item.track.id);
     return true;
   });
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.intersectionRatio > 0.8) {
-          handleReachEnd;
-        }
-      },
-      { threshold: [0.8] },
-    );
-
-    if (listRef.current) {
-      observer.observe(listRef.current);
-    }
-
-    return () => observer.disconnect();
-  }, []);
-
   return (
-    <div>
-      <AlbumContainer ref={listRef}>
+    <>
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        freeMode={true}
+        pagination={{ clickable: true }}
+        modules={[FreeMode, Pagination]}
+        className="swiper"
+        onReachEnd={handleReachEnd}
+      >
         {uniqueData.map((item) => (
-          <AlbumItem
-            key={item.track.id}
-            onClick={() => playUri(item.track.uri)}
-          >
-            <AlbumImageThumb>
-              <AlbumImage
-                src={
-                  item.track.album.images[0]?.url ||
-                  'https://via.placeholder.com/150'
-                }
-                alt={item.track.name}
-              />
-            </AlbumImageThumb>
-            <AlbumInfo>
-              <div>{item.track.name}</div>
-              <div>
-                {item.track.artists.map((artist, index) => (
-                  <ArtistName key={artist.id}>
-                    {artist.name}
-                    {index < item.track.artists.length - 1 && ', '}
-                  </ArtistName>
-                ))}
+          <SwiperSlide key={item.played_at}>
+            <div className="card" onClick={() => playUri(item.track.uri)}>
+              <div className="thumb">
+                <img
+                  src={
+                    item.track.album.images[0]?.url ||
+                    'https://via.placeholder.com/150'
+                  }
+                  alt={item.track.name}
+                />
               </div>
-            </AlbumInfo>
-          </AlbumItem>
+              <div className="text">
+                <div className="tit">{item.track.name}</div>
+                <div className="txt">
+                  {item.track.artists.map((artist, index) => (
+                    <Link to="" key={artist.id}>
+                      {artist.name}
+                      {index < item.track.artists.length - 1 && ', '}
+                    </Link>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
         ))}
-      </AlbumContainer>
-    </div>
+      </Swiper>
+    </>
   );
 };
 
